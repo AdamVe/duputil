f2455db09e2bd859d2ff628655e89e3d366383ed
Adam Velebil <pardalek@gmail.com>
2023-04-08 17:08:31 +0200

    initial

---
+++ duputil.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+get_auth() {
+    echo $(secret-tool lookup backup-name "$1")
+}
+
+if [[ "$#" -lt 4 ]]; then
+    echo "Usage: `basename $0` command remote-root backup-name local-dir"
+    exit -1
+fi
+
+COMMAND="${1}" REMOTE_ROOT="${2}"
+BACKUP_NAME="${3}"
+LOCAL_DIR="${4}"
+REMOTE_DEST="${REMOTE_ROOT}/${BACKUP_NAME}"
+
+if [[ ! -d "${LOCAL_DIR}" ]]; then
+    echo "Dir ${LOCAL_DIR} not found"
+    exit -1
+fi
+
+export PASSPHRASE=$(secret-tool lookup backup-name "${BACKUP_NAME}")
+
+if [ "${COMMAND}" == "v" ]; then
+    duplicity verify --compare-data -v3 --progress "${REMOTE_DEST}" "${LOCAL_DIR}"
+elif [ "${COMMAND}" == "vl" ]; then
+    duplicity verify -v3 --progress "${REMOTE_DEST}" "${LOCAL_DIR}"
+elif [ "${COMMAND}" == "b" ]; then
+    duplicity --progress -v3 "${LOCAL_DIR}" "${REMOTE_DEST}"
+else
+    echo "Invalid command ${COMMAND}"
+fi
