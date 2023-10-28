40ff47bb57e305f6a12c472ec5b61e4f598e9b98
Adam Velebil <pardalek@gmail.com>
2023-10-28 14:40:14 +0200

    unify log level to 8/info

--- duputil.sh
+++ duputil.sh
@@ -22,13 +22,13 @@ fi
 export PASSPHRASE=$(secret-tool lookup backup-name "${BACKUP_NAME}")
 
 if [ "${COMMAND}" == "v" ]; then
-    duplicity verify --compare-data -v3 --progress "${REMOTE_DEST}" "${LOCAL_DIR}"
+    duplicity verify --compare-data -v8 --progress "${REMOTE_DEST}" "${LOCAL_DIR}"
 elif [ "${COMMAND}" == "vl" ]; then
-    duplicity verify -v3 --progress "${REMOTE_DEST}" "${LOCAL_DIR}"
+    duplicity verify -v8 --progress "${REMOTE_DEST}" "${LOCAL_DIR}"
 elif [ "${COMMAND}" == "b" ]; then
-    duplicity --progress -v3 "${LOCAL_DIR}" "${REMOTE_DEST}"
+    duplicity --progress -v8 "${LOCAL_DIR}" "${REMOTE_DEST}"
 elif [ "${COMMAND}" == "r" ]; then
-    duplicity restore -vi "${REMOTE_DEST}" "${LOCAL_DIR}"
+    duplicity restore -v8 "${REMOTE_DEST}" "${LOCAL_DIR}"
 else
     echo "Invalid command ${COMMAND}"
 fi
