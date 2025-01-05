6a8f84ccee0b5e6f370310c30100b0dcfeab114e
Adam Velebil <pardalek@gmail.com>
2025-01-05 11:27:06 +0100

    minor fixes

--- duputil.sh
+++ duputil.sh
@@ -9,7 +9,8 @@ if [[ "$#" -lt 4 ]]; then
     exit -1
 fi
 
-COMMAND="${1}" REMOTE_ROOT="${2}"
+COMMAND="${1}"
+REMOTE_ROOT="${2}"
 BACKUP_NAME="${3}"
 LOCAL_DIR="${4}"
 REMOTE_DEST="${REMOTE_ROOT}/${BACKUP_NAME}"
@@ -26,9 +27,13 @@ export PASSPHRASE=$(secret-tool lookup backup-name "${BACKUP_NAME}")
 if [ "${COMMAND}" == "v" ]; then
     duplicity verify --compare-data -v${VERBOSITY} --progress "${REMOTE_DEST}" "${LOCAL_DIR}"
 elif [ "${COMMAND}" == "b" ]; then
-    duplicity --full-if-older-than 6M --progress -v${VERBOSITY} "${LOCAL_DIR}" "${REMOTE_DEST}"
+    duplicity backup --full-if-older-than 6M --progress --progress-rate 5 -v${VERBOSITY} "${LOCAL_DIR}" "${REMOTE_DEST}"
+elif [ "${COMMAND}" == "dry-run" ]; then
+    duplicity backup --full-if-older-than 6M --dry-run -v${VERBOSITY} "${LOCAL_DIR}" "${REMOTE_DEST}"
 elif [ "${COMMAND}" == "r" ]; then
     duplicity restore -v${VERBOSITY} "${REMOTE_DEST}" "${LOCAL_DIR}"
+elif [ "${COMMAND}" == "st" ]; then
+    duplicity collection-status -v${VERBOSITY} "${REMOTE_DEST}"
 else
     echo "Invalid command ${COMMAND}"
 fi
